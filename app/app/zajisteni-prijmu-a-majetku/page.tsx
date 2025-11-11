2d86cf5b454509eec203700f020a7ee82dafbbd6
patyzonoffreedom98 <patyzon.of.freedom98@gmail.com>
2025-11-11 12:18:18 +0100

    Create page.tsx

---
+++ app/app/zajisteni-prijmu-a-majetku/page.tsx
@@ -0,0 +1,9 @@
+// app/zajisteni-prijmu-a-majetku/page.tsx
+export default function Zajisteni(){
+  return (
+    <div style={{border:'1px solid rgba(255,255,255,.15)', borderRadius:16, padding:20}}>
+      <h1 style={{marginTop:0}}>Zajištění příjmu a majetku</h1>
+      <p>Zde bude text k invaliditě, nemocenské, majetku (domácnost, nemovitost) a odpovědnosti.</p>
+    </div>
+  );
+}
