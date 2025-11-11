24cdcd52125bc4264475b7e7ebbd3aedd4e70eb7
patyzonoffreedom98 <patyzon.of.freedom98@gmail.com>
2025-11-11 11:07:30 +0100

    Create page.tsx

---
+++ app/sluzby/page.tsx
@@ -0,0 +1,6 @@
+export default function Page(){
+  return <main style={{maxWidth:980,margin:"0 auto",padding:"32px 20px"}}>
+    <h1>Služby</h1>
+    <p>Hypotéky, investice, zajištění příjmu, podnikatelská rizika, zaměstnanecké benefity.</p>
+  </main>;
+}
