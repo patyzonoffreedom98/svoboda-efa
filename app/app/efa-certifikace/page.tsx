aa51586c2604aa15ea95d65928004342be2427ed
patyzonoffreedom98 <patyzon.of.freedom98@gmail.com>
2025-11-11 12:15:31 +0100

    Create page.tsx

---
+++ app/app/efa-certifikace/page.tsx
@@ -0,0 +1,9 @@
+// app/efa-certifikace/page.tsx
+export default function EFA(){
+  return (
+    <div style={{border:'1px solid rgba(255,255,255,.15)', borderRadius:16, padding:20}}>
+      <h1 style={{marginTop:0}}>EFA certifikace</h1>
+      <p>Zde bude text k EFA – co dokládá, v čem je přínos, co klient získá.</p>
+    </div>
+  );
+}
