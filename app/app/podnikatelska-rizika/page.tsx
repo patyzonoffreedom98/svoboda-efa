383a0671dc72bd473d6c3b332d574a724073cd95
patyzonoffreedom98 <patyzon.of.freedom98@gmail.com>
2025-11-11 18:13:21 +0100

    Delete app/app directory

--- app/app/podnikatelska-rizika/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/podnikatelska-rizika/page.tsx
-export default function Firmy(){
-  return (
-    <div style={{border:'1px solid rgba(255,255,255,.15)', borderRadius:16, padding:20}}>
-      <h1 style={{marginTop:0}}>Podnikatelská rizika</h1>
-      <p>Zde bude text k odpovědnosti firmy, majetku, přerušení provozu a flotilám.</p>
-    </div>
-  );
-}
