2977543ee7648e9e91ab418d2cc428a887805b72
patyzonoffreedom98 <patyzon.of.freedom98@gmail.com>
2025-11-11 11:08:06 +0100

    Create page.tsx

---
+++ app/sluzby/app/kalkulacky/page.tsx
@@ -0,0 +1,6 @@
+export default function Page(){
+  return <main style={{maxWidth:980,margin:"0 auto",padding:"32px 20px"}}>
+    <h1>Kalkulačky</h1>
+    <p>Na homepage najdete Investiční, Hypoteční a Cestu k rentě. Brzy přidáme další.</p>
+  </main>;
+}
