4651661a7596257d8ea2e750cc840502adf7eb2d
patyzonoffreedom98 <patyzon.of.freedom98@gmail.com>
2025-11-12 21:26:53 +0100

    Update InstagramEmbed.tsx

--- components/InstagramEmbed.tsx
+++ components/InstagramEmbed.tsx
@@ -1,30 +1,32 @@
 "use client";
 
 import { useMemo } from "react";
+import Link from "next/link";
 
 type Props = {
   /** URL na příspěvek: https://www.instagram.com/reel/XXXX/ nebo https://www.instagram.com/p/XXXX/ */
   url: string;
   /** Volitelný popisek pod náhledem */
   caption?: string;
-  /**
-   * Poměr stran (CSS aspect-ratio). Např. "4 / 5" (post/reel příjemně menší),
-   * "1 / 1" (čtverec) nebo "9 / 16" (dlouhé Reels).
-   */
+  /** Poměr stran (CSS aspect-ratio), např. "1 / 1", "4 / 5", "9 / 16" */
   aspect?: string;
   /** Max šířka embedu v px (jen kosmetika) */
   maxWidth?: number;
+  /** Odkaz na TVŮJ profil pro tlačítko "Zobrazit profil" */
+  profileUrl?: string;
 };
 
 export default function InstagramEmbed({
   url,
   caption,
-  aspect = "4 / 5",
+  aspect = "1 / 1",
   maxWidth,
+  profileUrl,
 }: Props) {
   const embedUrl = useMemo(() => {
     try {
       const u = new URL(url);
+      // Instagram akceptuje /embed na konci (funguje pro /reel/ i /p/)
       if (!/\/embed\/?$/.test(u.pathname)) {
         u.pathname = u.pathname.replace(/\/$/, "") + "/embed";
       }
@@ -70,9 +72,24 @@ export default function InstagramEmbed({
         />
       </div>
 
-      {caption && (
-        <div className="small" style={{ padding: 12 }}>{caption}</div>
-      )}
+      {/* Popisek + tlačítko na TVŮJ profil */}
+      <div
+        style={{
+          display: "flex",
+          gap: 8,
+          alignItems: "center",
+          justifyContent: "space-between",
+          padding: 12,
+          flexWrap: "wrap",
+        }}
+      >
+        {caption && <div className="small">{caption}</div>}
+        {profileUrl && (
+          <Link href={profileUrl} target="_blank" className="btn btn-outline">
+            Zobrazit profil
+          </Link>
+        )}
+      </div>
     </div>
   );
 }
