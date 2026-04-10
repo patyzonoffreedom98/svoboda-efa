8e78793d18803f15ab21773ba3a3a0ea3980d2fc
patyzonoffreedom98 <patyzon.of.freedom98@gmail.com>
2026-04-10 22:22:46 +0200

    Create head.tsx

---
+++ app/hypoteka/head.tsx
@@ -0,0 +1,11 @@
+export default function Head() {
+  return (
+    <>
+      <title>Hypotéka | Bc. Patrik Svoboda, EFA</title>
+      <meta
+        name="description"
+        content="Pomohu Vám s novou hypotékou, refinancováním i nastavením financování tak, aby dávalo smysl dnes i do budoucna."
+      />
+    </>
+  );
+}
